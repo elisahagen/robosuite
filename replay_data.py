fix averaged action

replay_data.py
@@ -6,8 +6,8 @@ from custom_assets.BinToBinTransfer import BinToBinTransfer
 from datetime import datetime
 import time
 
-base_dir = "/home/elisa/Documents/data/robosuite_automated/teleop_dataset_1_20250731_172913/"  
-demo_file = os.path.join(base_dir, "teleop_demo.json")
+base_dir = "/home/elisa/Documents/data/robosuite_automated/teleop_dataset_1_20250801_151217/"  
+demo_file = os.path.join(base_dir, "teleop_demo")
 with open(demo_file, "r") as f:
     demo_data = json.load(f)["data"]
 
@@ -35,12 +35,9 @@ env = BinToBinTransfer(
     initialization_noise=None,  
 )
 
-def avg_actions(buf):
-    return np.mean(np.stack(buf, axis=0), axis=0)
 
 obs = env.reset()
-action_buffer = []
-SMOOTH_WINDOW = 5
+
 obj_body_id = env.sim.model.body_name2id("Bread_main")  
 
 env.sim.model.body_pos[obj_body_id] = bread_pos
@@ -52,18 +49,8 @@ env.sim.forward()
 
 for i, step_data in enumerate(demo_data):
     time.sleep(0.3)
-    raw_action = np.array(step_data["action"])
-    action_buffer.append(raw_action)
-    
-    # compute the smoothed action
-    if len(action_buffer) < SMOOTH_WINDOW:
-        # until the buffer is “full” you can just replay raw
-        action_to_apply = raw_action
-    else:
-        action_to_apply = avg_actions(action_buffer)
-    
-    # step with the smoothed action
-    obs, reward, done, _ = env.step(action_to_apply)
+    action = np.array(step_data["action"])
+    obs, reward, done, _ = env.step(action)
 
     print(f"Step {i}: Reward={reward}, Done={done}")
 